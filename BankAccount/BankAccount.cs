2047aa48c17bfc42ef56b8eb290909ec9fe43b55
Wolvye <alainsoltau@googlemail.com>
2025-03-26 11:49:38 +0100

    Update

---
+++ BankAccount/BankAccount.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BankAccount
+{
+    internal class BankAccount
+    {
+        public string Number { get; private set; }
+        public string Owner { get; private set; }
+        public float Balance { get; private set; }
+        public BankAccount(string number, string owner, float initalBalance)
+        {
+            Number = number;
+            Owner = owner;
+            Balance = initalBalance;
+        }
+        public void MakeDeposit(float amount)
+        {
+            Balance += amount;
+        }
+        public void MakeWithdrawal(float amount)
+        {
+            if (amount <= Balance)
+            {
+                Balance -= amount;
+            }
+            else
+            {
+                Console.WriteLine("Not enough money in the account");
+            }
+        }
+        public void PrintBalance()
+        {
+            Console.WriteLine($"Account number: {Number}");
+            Console.WriteLine($"Account owner: {Owner}");
+            Console.WriteLine($"Account balance: {Balance}");
+        }
+    }
+}
--- BankAccount/Program.cs
+++ BankAccount/Program.cs
@@ -1,10 +1,50 @@
-﻿namespace BankAccount
+﻿using System.Threading.Channels;
+
+namespace BankAccount
 {
     internal class Program
     {
         private static void Main(string[] args)
         {
-            
+          //Uebung: Bankaccount erstellen.
+          
+            BankAccount account = new BankAccount("123-456", "John Doe", 1000);
+            {
+                while (true)
+                {
+                    Console.WriteLine("Bitte wählen Sie eine Aktion aus");
+                    Console.WriteLine("1. Kontostand anzeigen");
+                    Console.WriteLine("2. Einzahlung tätigen");
+                    Console.WriteLine("3. Auszahlung");
+                    Console.WriteLine("4. Beenden");
+
+                    string input = Console.ReadLine();
+                    if (input == "1")
+                    {
+                        account.PrintBalance();
+                    }
+                    else if (input == "2")
+                    {
+                        Console.WriteLine("Gib den Betrg ein, den du einzahlen willst: ");
+                        float depositAmount = float.Parse(Console.ReadLine());
+                        account.MakeDeposit(depositAmount);
+                    }
+                    else if (input == "3")
+                    {
+                        Console.WriteLine("Gib den Betrg ein, den du abheben willst: ");
+                        float withDrawAmount = float.Parse(Console.ReadLine());
+                        account.MakeWithdrawal(withDrawAmount);
+                    }
+                    else if (input == "4")
+                    {
+                        break;
+                    }
+                    else
+                    {
+                        Console.WriteLine("Ungültige Eingabe");
+                    }
+                }
+            }
         }
     }
 }
